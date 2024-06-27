c2b7e2e3ae15d0ebe44ca7cee37ded5b59e24575
Leandro Fonseca <leandrofonseca.ti@gmail.com>
2024-06-27 14:23:46 -0300

    ajustes

---
+++ ConectaEsporteSolution/ConectaEsporte.Uol/Entity/ConsultaTransacaoPagSeguroDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConectaEsporte.Uol.Entity
+{
+    public class ConsultaTransacaoPagSeguroDTO
+    {
+        public DateTime Date { get; set; }
+        public int ResultsInThisPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public List<ConsultaTransacaoPagSeguroTransactionDTO> listTransaction { get; set; }
+    }
+}
---
+++ ConectaEsporteSolution/ConectaEsporte.Uol/Entity/PagSeguroCompradorDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConectaEsporte.Uol.Entity
+{
+    public class PagSeguroCompradorDTO
+    {
+        public string SenderName { get; set; }
+        public string SenderAreaCode { get; set; }
+        public string senderPhone { get; set; }
+        public string senderEmail { get; set; }
+    }
+}
